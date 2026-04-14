04c18637242c3c01d71dbc783ed56529ab7b1df4
Sripadam Navya Sri <navyasripadam006@gmail.com>
2026-04-14 19:57:29 +0530

    fixed vite host issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  preview: {
+    host: true,
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      "stylesense-ai-frontend.onrender.com"
+    ]
+  }
 })
